65a59ead0d818a72b7daa60b717812939bdb3911
Samuel Majerus <ladolego@gmail.com>
2022-09-23 14:46:12 -0700

    'DealApplies' method, Test 1:  Good fail.

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -13,31 +13,34 @@ namespace Bakery.Tests
       Assert.AreEqual(false, test.DealApplies());
     }
 
+/*
     [TestMethod]
-    public void DealApplies_UseBranchingToCheckBreadDealCompatability_True()
+    public void DealApplies_UseBranchingToCheckPastryDealCompatability_True()
     {
-      Bread test1 = new Bread(1);
-      Bread test2 = new Bread(2);
-      Bread test3 = new Bread(3);
+      Pastry test1 = new Pastry(1);
+      Pastry test2 = new Pastry(2);
+      Pastry test3 = new Pastry(3);
       Assert.AreEqual(false, test1.DealApplies());
       Assert.AreEqual(false, test2.DealApplies());
       Assert.AreEqual(true, test3.DealApplies());
     }
+*/
 
+/*
     [TestMethod]
     public void DetermineTotalPrice_UseBranchingToMakeCalculationsBasedOnQuantity_True()
     { 
-      Bread test1 = new Bread(1); 
-      Bread test2 = new Bread(2); 
-      Bread test3 = new Bread(3); 
-      Bread test4 = new Bread(4);
-      Bread test5 = new Bread(8); 
+      Pastry test1 = new Pastry(1); 
+      Pastry test2 = new Pastry(2); 
+      Pastry test3 = new Pastry(3); 
+      Pastry test4 = new Pastry(4);
+      Pastry test5 = new Pastry(8); 
       Assert.AreEqual(5, test1.DetermineTotalPrice()); 
       Assert.AreEqual(10, test2.DetermineTotalPrice()); 
       Assert.AreEqual(10, test3.DetermineTotalPrice()); 
       Assert.AreEqual(15, test4.DetermineTotalPrice()); 
       Assert.AreEqual(30, test5.DetermineTotalPrice()); 
     }
-
+*/
   }
 }
