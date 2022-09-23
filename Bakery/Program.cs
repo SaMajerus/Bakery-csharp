49d89a6fabe0f88314a452c1759b9cc58dda158c
Samuel Majerus <ladolego@gmail.com>
2022-09-23 15:41:27 -0700

    Initial commit of Console-UI Logic file 'Program.cs'

---
+++ Bakery/Program.cs
@@ -0,0 +1,15 @@
+using System;
+//using System.Collections.Generic;
+using Bakery.Models;
+
+/*     Console-UI Logic     */ 
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
